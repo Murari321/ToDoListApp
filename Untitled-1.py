87a3d4b9c3cab2a5054dab6fbae39780c86e4fa0
Murari P <murari.p.130020@marwadiuniversity.ac.in@gmail.com>
2026-08-07 11:49:53 +0530

    Version 2.0 - Improved To Do List App

--- Untitled-1.py
+++ Untitled-1.py
@@ -7,85 +7,110 @@ def menu():
     print("Enter 6 to Mark task don")
     print("Enter 7 to Remove task")
     print("Enter 8 to Exit")
-    choice=int(input("Enter your choice:"))
-    return choice
+    try:
+        choice=int(input("Enter your choice:"))
+        return choice
+    except ValueError:
+        print("Invalid input")
+
+def read_data():
+    with open("to do.txt","r") as f:
+        data=f.readlines()
+        return data
+
+def write_data(data):
+    with open("to do.txt","w") as f:
+        f.writelines(data)
+
+def add_data(data):
+    with open("to do.txt","a") as f:
+        f.write("\n"+data)
+
 def view_task():
-    f=open("to do.txt","r")
-    data=f.read()
-    print(data)
-    f.close()
-    return
+    data=read_data()
+    if not data:
+        print("no tasks avalable")
+    else:
+        for i, task in enumerate(data):
+            print(f"{i+1}. {task.strip()}")
+
 def add_task():
-    f=open("to do.txt","a")
-    task=input("Enter the task to be added:")
-    f.write("\n"+task)
-    f.close()
-    print("Task is added")
-    return
+    task = input("Enter new task: ").strip()
+    old_data = read_data()
+    if task in [i.strip() for i in old_data]:
+        print("Task already exists")
+        return
+    if not old_data:
+        write_data([task])
+    else:
+        add_data(task)
+    print("Task added")
+
 def edit_task():
-    f=open("to do.txt","r")
-    data=f.read()
-    f.close()
-    task=input("entere the task to Edit:")
-    if(data.find(task)!=-1):
-        new_task=input("Enter the new task:")
-        new_data=data.replace(task,new_task)
-        print("Task is edited")
-        print("Enter the 1 to view all tasks")
-        f=open("to do.txt","w")
-        f.write(new_data)
-        f.close()
+    view_task()
+    try:
+        num = int(input("Enter task number: "))
+    except ValueError:
+        print("Please enter a valid number.")
+        return
+    index = num-1
+    data=read_data()
+    if 0 <= index < len(data):
+        new_task = input("Enter new task: ")
+        if new_task in [task.strip() for task in data]:
+            print("Task already exists.")
+            return
+        data[index] = new_task.strip() + "\n"
+        write_data(data)
+        print("Task updated successfully.")
     else:
-        print("task dosn't exist")
+        print("Invalid task number.")
         print("Enter 1 to view all tasks")
-    return
-def Search_task():
-    f=open("to do.txt","r")
-    data=f.read()
-    task=input("Enter the task to search:")
-    if(data.find(task)!=1):
-        print("Task is present")
-        print("Enter 1 if you want to view the lisk of tasks:")
-    else:
-        print("tast is not present")
-        print("Enter 1 to view all tasks")
-    f.close()
-    return
+
+def search_task():
+    data = read_data()
+    task = input("Enter the task to search: ").strip()
+    found = False
+    for i in range(len(data)):
+        if task.lower() == data[i].strip().lower():
+            print(f"Task found at position {i+1}")
+            found = True
+            break
+
+    if not found:
+        print("Task not found.")
+
 def save_task():
     print("task is saved")
     print("Enter 8 to exit")
     return
+
 def mark_done():
-    f = open("to do.txt", "r")
-    data = f.readlines()
-    f.close()
+    view_task()
+    num = int(input("Enter task number: "))
+    data=read_data()
+    index = num - 1
+    if 0 <= index < len(data):
+        if "(Done)" not in data[index]:
+            data[index] = data[index].strip() + " (Done)\n"
+            write_data(data)
+            print("Task marked as done.")
+        else:
+            print("Task is already marked as done.")
+    else:
+        print("Invalid task number.")
 
-    task = input("Enter task that is done: ")
-
-    for i in range(len(data)):
-        if task in data[i]:
-            data[i] = data[i].replace(task, task + " (Done)")
-
-    f = open("to do.txt", "w")
-    f.writelines(data)
-    f.close()
-    print("Done")
-    return
 def remove_task():
-    print("Enter the task to remove")
-    task=input()
-    f=open("to do.txt","r")
-    data=f.readlines()
-    f.close()
-
-    for i in range(len(data)):
-        if task in data[i]:
-            data[i]=data[i].replace(task,"")
-            print("Task removed")
-    f=open("to do.txt","w")
-    f.writelines(data)
-    f.close()
-    return
+    view_task()
+    num=int(input("Enter the task number to remove:"))
+    data=read_data()
+    if 1 <= num <= len(data):
+        index=num-1
+        data.pop(index)
+        write_data(data)
+        print("Task removed")
+    else:
+        print("Invalid Task Number")
 
 while True:
     match menu():
@@ -96,7 +121,7 @@ while True:
         case 3:
             edit_task()
         case 4:
-            Search_task()
+            search_task()
         case 5:
             save_task()
         case 6:
